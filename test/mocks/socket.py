2eebbd4573fbbbacce118297a32d9fb1cd0b30c3
Ian Carroll <ian.d.carroll@gmail.com>
2020-04-26 20:08:34 -0700

    Add a mock socket for debugging and eventual testing

---
+++ test/mocks/socket.py
@@ -0,0 +1,32 @@
+import random
+
+class socket:
+    def socket():
+        return socket()
+
+    def connect(self, host_port_tuple):
+        print(f'Fake socket connected: {host_port_tuple}')
+
+    def send(self, string):
+        print(f'Fake socket was sent: {string}')
+
+    def setblocking(self, int):
+        print(f'Fake socket set blocking: {int}')
+
+    def recv(self, int):
+        print(f'Fake socket tried to recieve a message in: {int}')
+        return fake_reception();
+
+    def decode(self, byte_array):
+        print(f'Fake socket tried to decode: {byte_array}')
+        return "This is your Fake Server Speaking: Be excellent to each other."
+
+class fake_reception:
+        def decode(fake_reception, encoding):
+            print(f'Fake reception tried to decode itself: {encoding}')
+
+            fake_server_message = 'This is your Fake Server Speaking: Be excellent to each other.'
+            special_ping_message = 'PING :tmi.twitch.tv\r\n'
+            message = random.choice([fake_server_message, special_ping_message])
+            print(message)
+            return message
