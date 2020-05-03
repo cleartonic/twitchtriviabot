ef682fdde35704f71b689737ea181e645b3f7bb2
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-03 00:07:43 -0700

    Import trivia sets and define what questions look like for the game

---
+++ src/triviaset.py
@@ -0,0 +1,43 @@
+import pandas
+import math
+
+class Trivia_Set:
+    required_columns = ['Round', 'Question', 'Answer']
+
+    def __init__(self, csv_file):
+        dataFrame = pandas.read_csv(csv_file)
+        self.columns = dataFrame.to_dict()
+        self.questions = dataFrame.to_dict('record')
+        self.validate_columns(self.columns)
+        self.validate_rounds(self.columns['Round'])
+
+    def validate_columns(self, columns):
+        for column in Trivia_Set.required_columns:
+            for key, value in columns[column].items():
+                if type(value) != str and math.isnan(value):
+                    self.report_problem(f'It looks like there\'s an empty {column} on question { key + 1 } in the triviaset file')
+
+    def validate_rounds(self, rounds):
+        round_list = []
+        for key, round in rounds.items():
+            if type(round) != int:
+                self.report_problem(f'It looks like at least one of the "Rounds" in the triviaset file is not an integer')
+            if round not in round_list:
+                round_list.append(round)
+        round_list = sorted(round_list)
+        if round_list[0] != 1:
+            self.report_problem('It looks like either there are no questions in round 1 or you made a round 0 or something')
+        if round_list[len(round_list) - 1] != len(round_list):
+            self.report_problem('It looks like you skipped a round somewhere.')
+
+
+    def report_problem(self, message):
+        print('\n\tLuke, we\'ve got a malfunction in fire control...\n')
+        print(message)
+        print('Better go fix the triviaset.csv file before trying to shoot this thing off')
+        exit()
+
+    def get_questions(self):
+        return self.questions
+
+print(Trivia_Set("triviaset.csv").get_questions())
---
+++ test/mocks/questions.py
@@ -0,0 +1,22 @@
+
+columns = {
+    'Round': {0: 1, 1: 2, 2: 2, 3: 2, 4: 3},
+    'Question': {
+        0: "What's a Diorama?",
+        1: 'What is your name?',
+        2: 'What is your quest?',
+        3: 'What is your favorite color?',
+        4: 'Are you a god?'},
+    'Answer': {
+        0: "OMG Han! Chewie! They're all here!",
+        1: 'Sir Lancelot of Camelot',
+        2: 'To seek the Holy Grail',
+        3: 'Blue', 
+        4: 'YES!'}}
+
+questions = [
+    {'Round': 1, 'Question': "What's a Diorama?", 'Answer': "OMG Han! Chewie! They're all here!"},
+    {'Round': 2, 'Question': 'What is your name?', 'Answer': 'Sir Lancelot of Camelot'},
+    {'Round': 2, 'Question': 'What is your quest?', 'Answer': 'To seek the Holy Grail'},
+    {'Round': 2, 'Question': 'What is your favorite color?', 'Answer': 'Blue'},
+    {'Round': 3, 'Question': 'Are you a god?', 'Answer': 'YES!'}]
