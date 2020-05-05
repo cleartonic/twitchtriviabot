3fc3621498c59582db3f7cea5493875ff8942562
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-04 20:19:32 -0700

    Commander only cares about useful commands

--- src/commander.py
+++ src/commander.py
@@ -1,17 +1,10 @@
 class Commander:
     commands = [
         "!triviastart",
+        "!triviastartround",
+        "!nextround",
         "!triviaend",
-        "!top3",
-        "!hint",
-        "!bonus",
-        "!score",
-        "!next",
-        "!stop",
-        "!loadconfig",
-        "!backuptrivia",
-        "!loadtrivia",
-        "!creator"
+        "!top3"
     ]
 
     def __init__(self, admins, commands):
@@ -22,45 +15,26 @@ class Commander:
         message = reply[1]
         pass
 
-    def score():
-        pass
-
-    def leader_board():
-        pass
-
-    def useage():
-        pass
-
-    def stop_bot(username):
-        pass
+    def load_trivia():
+        pass # what if this is just checked when trivia is started?
 
     def start_game(username):
+        pass
+
+    def start_round(username):
         pass
 
     def next_round(username):
         pass
 
-    def next_question(username):
+    def end_game(username):
         pass
 
-    def end_game(username):
+    def leaderBoard(username):
         pass
 
     def hint(username):
         pass
 
-    def bonus(username):
-        pass # how do you even use this?
-
-    def load_config():
-        pass # what if this always works?
-
     def backup_trivia():
         pass # what if this is just auto-backed up?
-
-    def load_trivia():
-        pass # what if this is just checked when trivia is started?
-        # what if you can load the round this way?
-
-    def creator():
-        pass # how useful is this?
