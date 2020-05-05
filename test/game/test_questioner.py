e052dee9d8cfe4af6b8067fcadeed27b90869589
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-04 20:37:48 -0700

    Rename test descriptions

--- test/game/test_questioner.py
+++ test/game/test_questioner.py
@@ -2,7 +2,7 @@ import unittest
 from src.game.questioner import Questioner as Subject
 
 class QuestionerTestCase(unittest.TestCase):
-    def test_trivium_knows_itself(self):
+    def test_questioner_knows_itself(self):
         question = {
             'Round': 1,
             'Ask': "What's a Diorama?",
@@ -12,7 +12,7 @@ class QuestionerTestCase(unittest.TestCase):
         self.assertEqual(s.ask, "What's a Diorama?")
         self.assertEqual(s.answer, "OMG Han! Chewie! They're all here!")
 
-    def test_trivium_accepts_arbitrary_values_since_its_validated_elsewhere(self):
+    def test_questioner_accepts_arbitrary_values_since_its_validated_elsewhere(self):
         question = {
             'Round': "Awesome",
             'Ask': 24601,
@@ -22,7 +22,7 @@ class QuestionerTestCase(unittest.TestCase):
         self.assertEqual(s.ask, 24601)
         self.assertEqual(s.answer, False)
 
-    def test_trivium_doesnt_care_if_there_are_extra_fields(self):
+    def test_questioner_doesnt_care_if_there_are_extra_fields(self):
         question = {
             'Round': 1,
             'Ask': "What's a Diorama?",
