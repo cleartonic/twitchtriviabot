27ab8f225eb54133089d7d3b5960cd66ac4dd193
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-18 23:01:28 -0700

    Refactor list_by_rounds

--- src/game/game.py
+++ src/game/game.py
@@ -10,23 +10,23 @@ class Game:
         self.rounds = self.init_rounds(questions)
 
     def init_rounds(self, questions):
-        game_questions = self.list_by_rounds(questions)
-        return [self.init_r(round_questions) for round_questions in game_questions]
+        game_qs = self.list_by_rounds(questions)
+        return [self.init_r(round_questions) for round_questions in game_qs]
 
     def list_by_rounds(self, questions):
-        game_questions = []
-        for question in questions:
-            if len(game_questions) == 0:
-                game_questions.append([question])
-            else:
-                question_appended = False
-                for round_questions in game_questions:
-                    if question['Round'] == round_questions[0]['Round']:
-                        round_questions.append(question)
-                        question_appended = True
-                if not question_appended:
-                    game_questions.append([question])
-        return game_questions
+        game_qs = []
+        for q in questions:
+            self.add(q, game_qs) if len(game_qs) != 0 else game_qs.append([q])
+        return game_qs
+
+    def add(self, question, game_questions):
+        question_is_the_first_in_a_new_round = True
+        for round_questions in game_questions:
+            if question['Round'] == round_questions[0]['Round']:
+                round_questions.append(question)
+                question_is_the_first_in_a_new_round = False
+        if question_is_the_first_in_a_new_round:
+            game_questions.append([question])
 
     def init_r(self, round_questions):
         return self.round(self.questioner, round_questions, self.connection)
