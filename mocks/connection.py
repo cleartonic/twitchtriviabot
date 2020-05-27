5210f16fcb5809564143919bd0b69531b665e7e2
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-27 12:53:28 -0700

    Make the tests faster by turbo-charging the mock connection

--- mocks/connection.py
+++ mocks/connection.py
@@ -4,7 +4,7 @@ class Connection:
         self._message = 'No message recieved.'
         self._message_list = []
         self.last_response = ('bot', 'No Messages Recieved')
-        self.seconds_per_message = 1 / 1000
+        self.seconds_per_message = 1 / 100000
 
     def send(self, message):
         self._message_list.append(message)
