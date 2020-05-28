572dd9a267c381588e9a5c3cacec68e313459eaf
Ian Carroll <ian.d.carroll@gmail.com>
2020-05-27 18:19:12 -0700

    Make blank files for all the stuff that needs to be tested

---
+++ test/game/test_players.py
@@ -0,0 +1,29 @@
+import unittest
+
+class PlayersTestCase(unittest.TestCase):
+    def skip_test_players_score_adds_new_players_to_the_board(self):
+        self.assertEqual(True, True)
+
+    def skip_test_players_score_ups_an_existing_players_score(self):
+        pass
+
+    def skip_test_players_winner_adds_a_game_win_to_a_player(self):
+        pass
+
+    def skip_test_players_new_round_clears_all_old_round_scores(self):
+        pass
+
+    def skip_test_players_new_game_clears_all_old_round_scores(self):
+        pass
+
+    def skip_test_players_new_game_clears_all_old_game_scores(self):
+        pass
+
+    def skip_test_players_round_winners_gives_the_top_3_round_players(self):
+        pass
+
+    def skip_test_players_game_winners_gives_the_top_3_game_players(self):
+        pass
+
+    def skip_test_players_top_players_gives_the_top_3_round_players(self):
+        pass
