549daab305dbff96c8869b381ee069da7d2bd68a
Ian Carroll <ian.d.carroll@gmail.com>
2020-06-05 22:06:58 -0700

    Commander will recieve new messages from the connection

--- src/commander.py
+++ src/commander.py
@@ -1,3 +1,4 @@
+import time
 from src.mr_clean import Mr
 from .messages import Log as report
 from src.messages import Chat
@@ -10,38 +11,45 @@ from src.game.round import Round
 from src.game.game import Game
 
 class Commander:
-    commands = {
-        "start_the_next_trivia_round": "!go",
-        "stop_the_bot": "!stop"
-    }
-
-    def __init__(self, admins, connection, log):
+    def __init__(self, admins, connection, log, sleep = time.sleep):
         self.log = log
+        self.sleep = sleep
         self.admins = [Mr.lower(admin) for admin in admins]
         self.connection = connection
         self.last_response = ('bot', 'No Messages Recieved')
 
     def listen_for_commands(self):
         while self.connection.keep_IRC_running:
-            time.sleep(self.connection.seconds_per_message)
-            if self.last_response != self.connection.last_response:
-                self.last_response = self.connection.last_response
-                username = Mr.lower(self.last_response[0])
-                message = Mr.clean(self.last_response[1])
+            self.check_connection_last_message()
 
-                if message == commands['start_the_next_trivia_round']:
-                    self.go(username)
-                if message == commands['stop_the_bot']:
-                    self.stop(username)
+    def check_connection_last_message(self):
+        self.sleep(self.connection.seconds_per_message)
+        if self.last_response != self.connection.last_response:
+            self.respond_to_new_last_message()
 
-    def go(self, username):
+    def respond_to_new_last_message(self):
+        self.last_response = self.connection.last_response
+        username = Mr.lower(self.last_response[0])
+        message = Mr.clean(self.last_response[1])
+
+        commands = {
+            "!go": self.start_the_next_trivia_round,
+            "!stop": self.stop_the_bot
+        }
+
+        if message == commands["!go"]:
+            commands["!go"](username)
+        if message == commands["!stop"]:
+            commands["!stop"](username)
+
+    def start_the_next_trivia_round(self, username):
         command = commands['start_the_next_trivia_round']
         if username in self.admins:
             self.spin_up_a_trivia_round(username, command)
         else:
             self.log(report.bad_admin(username, command))
 
-    def stop(self, username):
+    def stop_the_bot(self, username):
         command = commands['stop_the_bot']
         if username in self.admins:
             self.graceful_shutdown(username, command)
--- test/test_commander.py
+++ test/test_commander.py
@@ -1,11 +1,36 @@
 import unittest
 from mocks.connection import Connection
+from mocks.socket import socket
+from mocks.time import Time
 from mocks.silent_log import dont_print
+from mocks.spy_log import Spy_Log
 from src.commander import Commander as Subject
 
 class CommanderTestCase(unittest.TestCase):
     def test_commander_initializes_with_no_messages_recieved(self):
         no_admins = []
-        mock_connection = Connection
-        s = Subject(no_admins, mock_connection, dont_print)
+        s = Subject(no_admins, Connection(), dont_print)
         self.assertEqual(s.last_response, ('bot', 'No Messages Recieved'))
+
+    def test_commander_case_insensitizes_admin_names(self):
+        fancy_admins = [
+            "dont_change_my_name",
+            "PlEaSe_cHaNgE_Me",
+            "!#@$_ni!_:="
+        ]
+        s = Subject(fancy_admins, Connection(), dont_print)
+        self.assertEqual(s.admins[0], fancy_admins[0])
+        self.assertEqual(s.admins[1], "please_change_me")
+        self.assertEqual(s.admins[2], fancy_admins[2])
+
+    def test_commander_initializes_with_no_messages_recieved(self):
+        test_response = ("gandhi", "Be the change you want to see in the world.")
+        no_admins = []
+        mock_connection = Connection()
+        mock_time = Time(dont_print).sleep
+        s = Subject(no_admins, mock_connection, dont_print, mock_time)
+
+        mock_connection.last_response = test_response
+        s.check_connection_last_message()
+
+        self.assertEqual(s.last_response, test_response)
